75b9abdd26f411f67c22c45540058323d5f4c19b
sumsky108 <sumendrakadek108@gmail.com>
2019-08-23 10:23:18 +0700

    First Commit

---
+++ src/arraylist.kt
@@ -0,0 +1,23 @@
+import java.util.*
+
+fun main (args : Array<String>) {
+    pengenalan()
+}
+fun pengenalan (){
+    var scan= Scanner(System.`in`)
+    var nama=""
+    var kelas=""
+    var alamat=""
+    var cita=""
+    var nim=""
+    println("masukkan nama")
+    nama=scan.next();
+    println("masukkan kelas")
+    kelas=scan.next()
+    println("masukkan nim")
+    nim=scan.next()
+    println("masukkan alamat")
+    alamat=scan.next()
+    println("masukkan cita cita")
+    cita=scan.next()
+}
